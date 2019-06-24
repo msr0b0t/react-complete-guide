03a497887131c802662ea30366108dbb2d148458
Mary Kouvela <marykouvela@gmail.com>
2019-06-24 13:10:51 +0300

    investigate components' lifecycle

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,7 +1,24 @@
-import React from "react";
+import React, {useEffect} from "react";
 import classes from "./Cockpit.css"
 
 const cockpit = (props) => {
+    useEffect(() => {
+        console.log("[Cockpit.js] useEffect");
+        setTimeout(() => {
+            alert("Saved data to cloud!");
+        }, 1000);
+        return () => {
+            console.log("[Cockpit.js] cleanup work in useEffect");
+        };
+    }, []);
+
+    useEffect(() => {
+        console.log("[Cockpit.js] 2nd useEffect");
+        return () => {
+            console.log("[Cockpit.js] cleanup work in 2nd useEffect");
+        };
+    });
+
     const assignedClasses = [];
     let btnClass = "";
     if (props.showPersons) {
@@ -17,13 +34,14 @@ const cockpit = (props) => {
 
     return (
         <div className={classes.Cockpit}>
-            <h1>Hi, I'm a React App</h1>
+            <h1>{props.title}</h1>
 		    <p className={assignedClasses.join(" ")}>This is really working!</p>
 			<button
 			    className={btnClass}
-			    onClick={props.clicked}>Toggle Persons</button>
+			    onClick={props.clicked}>Toggle Persons
+            </button>
         </div>
     );
 };
 
-export default cockpit;
+export default React.memo(cockpit);
--- src/containers/App.js
+++ src/containers/App.js
@@ -4,6 +4,11 @@ import Persons from "../components/Persons/Persons";
 import Cockpit from "../components/Cockpit/Cockpit";
 
 class App extends Component {
+	constructor(props) {
+		super(props);
+		console.log("[App.js] constructor");
+	}
+
 	state = {
 		persons: [
 			{id: "0", name: "Max", age: 28},
@@ -11,8 +16,31 @@ class App extends Component {
 			{id: "2", name: "Stephanie", age: 26}
 		],
 		otherState: "some other value",
-		showPersons: false
+		showPersons: false,
+		showCockpit: true
 	};
+
+	static getDerivedStateFromProps(props, state) {
+		console.log("[App.js] getDerivedStateFromProps", props);
+		return state;
+	}
+
+	// componentWillMount() {
+	// 	console.log("[App.js] componentWillMount");
+	// }
+
+	componentDidMount() {
+		console.log("[App.js] componentDidMount");
+	}
+
+	shouldComponentUpdate(nextProps, nextState) {
+		console.log("[App.js] shouldComponentUpdate");
+		return true;
+	}
+
+	componentDidUpdate() {
+		console.log("[App.js] componentDidUpdate");
+	}
 
 	deletePersonHandler = (personIndex) => {
 		// const persons = this.state.persons.slice();
@@ -43,21 +71,27 @@ class App extends Component {
 	}
 
 	render() {
+		console.log("[App.js] render");
 		let persons = null;
 
 		if (this.state.showPersons) {
 			persons = <Persons
 				persons={this.state.persons}
 				clicked={this.deletePersonHandler}
-				changed={this.nameChangeHandler}/>;
+				changed={this.nameChangeHandler}
+			/>;
 		}
 
 		return (
 			<div className={classes.App}>
-				<Cockpit
-					showPersons={this.state.showPersons}
-					persons={this.state.persons}
-					clicked={this.togglePersonsHandler}/>
+				<button onClick={() => this.setState({showCockpit: false})}>Remove Cockpit</button>
+				{this.state.showCockpit ?
+					(<Cockpit
+						title = {this.props.appTitle}
+						showPersons={this.state.showPersons}
+						persons={this.state.persons}
+						clicked={this.togglePersonsHandler}
+					/>) : null}
 				{persons}
 			</div>
 		);
