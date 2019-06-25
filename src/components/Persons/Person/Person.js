972dde72a86cdf50346dd5985b1369bd4a65a227
Mary Kouvela <marykouvela@gmail.com>
2019-06-25 09:33:17 +0300

    chenges

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,17 +1,43 @@
 import React, {Component} from "react";
 import classes from "./Person.css";
+import withClass from "../../../hoc/withClass";
+import Aux from "../../../hoc/Aux";
+import PropTypes from "prop-types";
+import AuthContext from "../../../context/auth-context";
 
 class Person extends Component {
+    constructor(props) {
+        super(props)
+        this.inputElementRef = React.createRef();
+    }
+
+    static contextType = AuthContext;
+
+    componentDidMount() {
+        this.inputElementRef.current.focus();
+        console.log(this.context.authenticated);
+    }
+
     render() {
         console.log("[Person.js] rendering...")
         return (
-            <div className={classes.Person}>
+            <Aux>
+                {this.context.authenticated ? (<p>Authenticated!</p>) : (<p>Please log in</p>)}
+            {/* <div className={classes.Person}> */}
                 <p onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old!</p>
                 <p>{this.props.children}</p>
-                <input type="text" onChange={this.props.changed} value={this.props.name}/>
-            </div>
+                <input ref={this.inputElementRef} type="text" onChange={this.props.changed} value={this.props.name}/>
+            {/* </div> */}
+            </Aux>
         );
     }
 };
 
-export default Person;
+Person.propTypes = {
+    click: PropTypes.func,
+    name: PropTypes.string,
+    age: PropTypes.number,
+    changed: PropTypes.func
+};
+
+export default withClass(Person, classes.Person);
--- src/containers/App.js
+++ src/containers/App.js
@@ -2,6 +2,9 @@ import React, {Component} from "react";
 import classes from "./App.css";
 import Persons from "../components/Persons/Persons";
 import Cockpit from "../components/Cockpit/Cockpit";
+import withClass from "../hoc/withClass";
+import Aux from "../hoc/Aux";
+import AuthContext from "../context/auth-context";
 
 class App extends Component {
 	constructor(props) {
@@ -17,7 +20,9 @@ class App extends Component {
 		],
 		otherState: "some other value",
 		showPersons: false,
-		showCockpit: true
+		showCockpit: true,
+		changeCounter: 0,
+		authenticated: false
 	};
 
 	static getDerivedStateFromProps(props, state) {
@@ -62,12 +67,18 @@ class App extends Component {
 		const persons = [...this.state.persons];
 		persons[personIndex] = person;
 
-		this.setState({persons: persons});
+		this.setState((prevState, props) => {
+			return {persons: persons, changeCounter: this.state.changeCounter + 1};
+		});
 	}
 
 	togglePersonsHandler = () => {
 		const doesShow = this.state.showPersons;
 		this.setState({showPersons: !doesShow});
+	}
+
+	loginHandler = () => {
+		this.setState({authenticated: true});
 	}
 
 	render() {
@@ -79,23 +90,26 @@ class App extends Component {
 				persons={this.state.persons}
 				clicked={this.deletePersonHandler}
 				changed={this.nameChangeHandler}
+				isAuthenticated={this.state.authenticated}
 			/>;
 		}
 
 		return (
-			<div className={classes.App}>
+			<Aux>
 				<button onClick={() => this.setState({showCockpit: false})}>Remove Cockpit</button>
-				{this.state.showCockpit ?
-					(<Cockpit
-						title = {this.props.appTitle}
-						showPersons={this.state.showPersons}
-						persons={this.state.persons}
-						clicked={this.togglePersonsHandler}
-					/>) : null}
-				{persons}
-			</div>
+				<AuthContext.Provider value={{authenticated: this.state.authenticated, login: this.loginHandler}}>
+					{this.state.showCockpit ?
+						(<Cockpit
+							title = {this.props.appTitle}
+							showPersons={this.state.showPersons}
+							persons={this.state.persons}
+							clicked={this.togglePersonsHandler}
+						/>) : null}
+					{persons}
+				</AuthContext.Provider>
+			</Aux>
 		);
 	}
 }
 
-export default App;
+export default withClass(App, classes.App);
